e08065f3736840a42a3ce403e3e299765b19edbe
srikantchaudhary <chaudharysrikant26@gmail.com>
2025-08-28 10:53:07 -0700

    Added some more lines of code

--- Zepto/Ecommerce dataset.sql
+++ Zepto/Ecommerce dataset.sql
@@ -66,3 +66,62 @@ having count(*) > 1
 order by count(*) desc;
 
 
+
+
+-- Data Cleaning Tasks
+
+-- 1. Identify and remove products with MRP = 0 or discounted price = 0.
+
+select count(*) from zepto where mrp != 0 or discountedsellingprice != 0;
+
+-- Remove
+DELETE FROM zepto
+WHERE mrp = 0 OR discountedsellingprice = 0;
+
+
+-- 2. Convert prices from paise to rupees.
+
+update zepto set mrp = mrp/100.00 , discountedSellingPrice = discountedSellingPrice/100.00;
+
+
+select * from zepto;
+
+
+-- 3. Ensure duplicate products are handled correctly.
+
+SELECT name, category, COUNT(*) AS duplicate_count
+FROM zepto
+GROUP BY name, category
+HAVING COUNT(*) > 1;
+
+
+
+-----------------  Questions  --------------------------
+
+-- 1. Find the top 10 best-value products based on the discount percentage.
+
+Select distinct name, mrp, discountPercent from zepto order by discountpercent desc limit 10;
+
+-- 2. What are the Products with High MRP but Out of Stock.
+
+select distinct name, category, mrp, outofstock from zepto 
+where outofstock = true 
+order by mrp desc limit 5;
+
+-- 3. Calculate Estimated Revenue for each Cateory.
+
+select category, sum(mrp) from zepto group by category;
+
+-- 4. Find all products where MRP is greater than 500 and discount is less than 10%.
+-- 5. Identify the top 5 categories offering the highest average discount percentage.
+-- 6. Find the price per gram for products above 100g and sort by best value.
+-- 7. Group the products into categories like LOW, Medium , Bulk.
+-- 8. What is the Total Inventory Weight Per Category.
+
+
+
+
+
+
+
+
